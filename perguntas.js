9d83c1ab35b986b3793f2ccfec8c3e944ed5089f
Schaab23 <147153403+Schaab23@users.noreply.github.com>
2024-09-12 16:12:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que faz a função len () em Python?',
+    'Retorna o número de itens em um objeto,como uma lista'
+)
+
+criaCartao(
+    'História',
+    'Em que ano os Portuguses chegaram ao Brasil?'
+    'Em 1500'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua espanhola',
+    'Como se diz oi em Espahol?',
+    'Oi em Espalhol é Holla'
+)
