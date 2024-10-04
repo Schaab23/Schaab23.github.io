25b0bc60c2ba99ac0c0f2a89e2d4c5d425d96b92
Schaab23 <147153403+Schaab23@users.noreply.github.com>
2024-10-04 20:03:59 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,60 +1,59 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Título 1',
+    'Pergunta 1',
+    'Resposta 1'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital do Brasil?',
-    'A capital do Brasil é Brasília'
+    'Título 2',
+    'Pergunta 2',
+    'Resposta 2'
 )
 
 criaCartao(
-    'Programação',
-    'Como fazer um comentario em Python?',
-    'Use o simbolo # antes do texto'
+    'Título 3',
+    'Pergunta 3',
+    'Resposta 3'
 )
 
 criaCartao(
-    'Lingua Espanhola',
-    'Como se diz oi em Espanhol?',
-    'Oi em Espanhol se fala Holla'
+    'Título 4',
+    'Pergunta 4',
+    'Resposta 4'
 )
 
 criaCartao(
-    'Lingua Inglesa',
-    'Como se diz cachorro em Inglês?',
-    'Cachorro em Inglês se diz Dog'
+    'Título 5',
+    'Pergunta 5',
+    'Resposta 5'
 )
 
 criaCartao(
-    'Robotica',
-    'O que é Robotica?',
-    'A ciência que estuda os robos,do desenho a montagem'
+    'Título 6',
+    'Pergunta 6',
+    'Resposta 6'
 )
 
 criaCartao(
-    'Arte',
-    'Quem pitou a Mona Lisa?',
-    'Leonardo da Vinci'
+    'Título 7',
+    'Pergunta 7',
+    'Resposta 7'
 )
 
 criaCartao(
-    'História',
-    'Quem escreveu Dom Quixote?',
-    'Miguel de Cervantes'
+    'Título 8',
+    'Pergunta 8',
+    'Resposta 8'
 )
-    
-criaCartao( 
-   'Esportes',
-    'Qual esporte é conhecido como esporte das multidões no Brasil?',
-    'É o futebol'
-    )
-    
+
 criaCartao(
-    'Televisão Brasileira',
-    'Qual apresentador é famoso por comandar o programa Caldeirão?',
-    'É o apresentador Luciano Huck'
+    'Título 9',
+    'Pergunta 9',
+    'Resposta 9'
 )
-    
+
+criaCartao(
+    'Título 10',
+    'Pergunta 10',
+    'Resposta 10'
+)
