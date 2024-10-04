e080d45113861f41e97098b03687899d9abf65bc
Schaab23 <147153403+Schaab23@users.noreply.github.com>
2024-10-04 20:17:17 -0300

    Add files via upload

---
+++ Schaab23-main/Schaab23-main/perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'Como fazer um comentario em Python?',
+    'Use o simbolo # antes do texto'
+)
+
+criaCartao(
+    'Lingua Espanhola',
+    'Como se diz oi em Espanhol?',
+    'Oi em Espanhol se fala Holla'
+
+)
+
+criaCartao(
+    `Lingua Inglesa`,
+    `Como se diz cachorro em Inglês?`
+    `Cachorro em Inglês se diz Dog`
+)
+
+criaCartao(
+    `Robotica`
+    `O que é Robotica?`
+    `A ciência que estuda os robos,do desenho a montagem`
+)
+
+criaCartao(
+    `Arte`
+    `Quem pitou a Mona Lisa?`
+    `Leonardo da Vinci`
+)
+
+criaCartao(
+    `História`
+    `Quem escreveu "Dom Quixote?`
+    `Miguel de Cervantes`
+)
